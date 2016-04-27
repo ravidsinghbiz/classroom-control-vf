a0bc72f1f56ddeb0bf00e27ebde37a364738e5df
Ravi D. Singh <ravidsinghbiz@users.noreply.github.com>
2016-04-27 02:08:45 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -53,6 +53,10 @@ node default {
   #}
   exec { "/usr/local/bin/cowsay 'Welcome to ${::fqdn}!' > /etc/motd":
     creates => '/etc/motd',
-    
   }
+  host { 'testing.puppetlabs.vm':
+    ensure => present,
+    ip => '127.0.0.1',
+  }
+}
 }
