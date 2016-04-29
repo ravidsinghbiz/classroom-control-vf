e49688014227a51a88d8a55e9b9c63c29ea4f15c
Ravi D. Singh <ravidsinghbiz@users.noreply.github.com>
2016-04-29 02:00:21 -0700

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,10 +1,12 @@
-class nginx {
+class nginx (
+  $root=undef,
+) {
   case $::osfamily {
     'redhat','debian' : {
       $package = 'nginx'
       $owner = 'root'
       $group = 'root'
-      $docroot = '/var/www'
+      $default_docroot = '/var/www'
       $confdir = '/etc/nginx'
       $logdir = '/var/log/nginx'
     }
@@ -12,13 +14,18 @@ class nginx {
       $package = 'nginx-service'
       $owner = 'Administrator'
       $group = 'Administrators'
-      $docroot = 'C:/ProgramData/nginx/html'
+      $default_docroot = 'C:/ProgramData/nginx/html'
       $confdir = 'C:/ProgramData/nginx'
       $logdir = 'C:/ProgramData/nginx/logs'
     }
     default : {
       fail("Module ${module_name} is not supported on ${::osfamily}")
     }
+  }
+
+  $docroot = $root ? {
+    undef => $default_docroot,
+    default => $root,
   }
 
   $user = $::osfamily ? {
